cf9aac0b336de1c7f61135e800afd5187d34918f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-02-02 23:17:23 -0500

    fix circular import

--- dpgen2/exploration/task/lmp_template_task_group.py
+++ dpgen2/exploration/task/lmp_template_task_group.py
@@ -16,9 +16,10 @@ from dpgen2.constants import (
     plm_input_name,
     plm_output_name,
 )
-
-from . import (
+from .conf_sampling_task_group import (
     ConfSamplingTaskGroup,
+)
+from .task import (
     ExplorationTask,
     ExplorationTaskGroup,
 )
